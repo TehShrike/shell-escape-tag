90a5a04dd1dd65965078d33d9db8852950d96d06
Josh Duff <me@JoshDuff.com>
2018-07-08 12:30:55 -0500

    Move the source code to the project root

--- test/require.js
+++ test/require.js
@@ -1,7 +1,7 @@
 const test = require(`zora`)
 
 test(`require without .default`, t => {
-	const shell = require(`../src/shell-escape-tag`)
+	const shell = require(`../`)
 	const files = [ `foo bar.gif`, `baz quux.png` ]
 	t.equal(shell`compress ${ files }`, `compress 'foo bar.gif' 'baz quux.png'`)
 })
